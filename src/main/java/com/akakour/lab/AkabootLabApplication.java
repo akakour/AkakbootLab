231b306a6c665f52e6b374e0eace5a5cb29e0257
Akakour <akakour@qq.com>
2020-02-19 00:10:12 +0800

    基础框架 web + lombok

---
+++ src/main/java/com/akakour/lab/AkabootLabApplication.java
@@ -0,0 +1,13 @@
+package com.akakour.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkabootLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkabootLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akakour/lab/AkabootLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akakour.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkabootLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
